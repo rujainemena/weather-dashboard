22bdd85dcd0812a0750dd28e18a29e2ebe2f91fb
rujainemena <135552275+rujainemena@users.noreply.github.com>
2023-12-05 23:56:53 -0500

    corrected js code and updated html elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,77 @@
-var apiKey="f7ae399b8e597d582a4791021dac9f31"
-var titleEl=document.getElementById("title")
-var tempEl=document.getElementById("temp")
-var windEl=document.getElementById("wind")
-var humidityEl=document.getElementById("humidity")
-var searchBtn=document.getElementById("search-btn")
-var cityInput=document.getElementById("city-input")
-var fivedayForcastEl= document.getElementById("fiveday-forcast")
+var apiKey = "f7ae399b8e597d582a4791021dac9f31";
+var titleEl = document.getElementById("title");
+var tempEl = document.getElementById("temp");
+var windEl = document.getElementById("wind");
+var humidityEl = document.getElementById("humidity");
+var searchBtn = document.getElementById("search-btn");
+var cityInput = document.getElementById("city-input");
+var fivedayForecast = document.getElementById("fiveday-forecast");
 
+// search button function
+function searchCity() {
+  var cityName = cityInput.value;
 
-function searchCity(){
-    var cityName=cityInput.value
-
-    displayWeather(cityName)
+  displayWeather(cityName);
 }
 
-function displayWeather(cityName){
-    var url="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+ apiKey+"&units=imperial"
+function displayWeather(cityName) {
+  // main weather display
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
 
-    fetch(url)
-    .then(function(response){
-        return response.json()
+  fetch(url)
+    .then(function (response) {
+      return response.json();
     })
-    .then(function(currentData){
-        console.log(currentData)
-        titleEl.innerHTML=currentData.name + dayjs.unix(currentData.dt).format(" (MM/DD/YYYY)")+ "<img src='https://openweathermap.org/img/wn/"+ currentData.weather[0].icon+"@2x.png'>"
+    .then(function (currentData) {
+      console.log(currentData);
+      titleEl.innerHTML =
+        currentData.name +
+        dayjs.unix(currentData.dt).format(" MM/DD/YYYY") +
+        "<img src='https://openweathermap.org/img/wn/" +
+        currentData.weather[0].icon +
+        "@2x.png'>";
+    });
+
+  // 5 day forecast display
+  var forecastUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=" +
+    apiKey +
+    "&units=imperial";
+
+  fetch(forecastUrl)
+    .then(function (response) {
+      return response.json();
     })
+    .then(function (forecastData) {
+      console.log(forecastData);
+      //grab every 12pm for each day for 5 days
+      var forecastArr = forecastData.list;
 
+      for (let i = 3, j = 1; i < forecastArr.length; i = i + 8, j++) {
+        console.log(forecastArr[i]);
 
-    var forecastUrl="https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey+"&units=imperial"
+        var cardTitle = document.getElementById("card-title" + j);
+        // console.log("card-title" + j);
+        // dynamic city title
+        cardTitle.textContent = dayjs.unix(forecastArr[i].dt).format("MM/DD/YYYY ");
 
-    fetch(forecastUrl)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(forecastData){
-        console.log(forecastData)
-        //grab every 12pm for each day for 5 days
-        var forecastArr=forecastData.list
-  
-        for (let i = 3,j=1; i < forecastArr.length; i=i+8,j++) {
-             console.log(forecastArr[i])
-               var cardTitle=document.getElementById("card-title"+j)
-               console.log("card-title"+j)
-               cardTitle.textContent=dayjs.unix(forecastArr[i].dt).format(" (MM/DD/YYYY)")
-               var temp=document.getElementById("temp"+j)
+        //   dynamic temperature 
+        var temp = document.getElementById("temp" + j);
+        temp.textContent = forecastArr[i].main.temp;
 
-               temp.textContent=forecastArr[i].main.temp
-        }
+        // dynamic wind
+        var wind = document.getElementById("wind" + j);
+        wind.textContent = forecastArr[i].wind.speed;
 
-    })
+        // dynamic humidity
+        var humidity = document.getElementById("humidity" + j);
+        humidity.textContent = forecastArr[i].main.humidity;
+      }
+    });
 }
 
-
-searchBtn.addEventListener("click", searchCity)
+// event listeners
+searchBtn.addEventListener("click", searchCity);
